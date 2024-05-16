383c9fda8152c61c31877c0ba72640e2d5ef3445
Orlando Batista <36862549+OBMGIS@users.noreply.github.com>
2024-05-16 00:17:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_da
 
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
-  
   // Once we get a response, send the data.features object to the createFeatures function
-  console.log(data.features);
   createFeatures(data.features);
 });
 
@@ -13,27 +11,21 @@ d3.json(queryUrl, function(data) {
 var legend = L.control({position: 'bottomleft'});
 
 legend.onAdd = function () {
+  var div = L.DomUtil.create('div', 'info legend'),
+      grades = [1, 2, 3, 4, 5],
+      labels = [];
 
-    var div = L.DomUtil.create('div', 'info legend'),
-        grades = [1,2,3,4,5],
-        labels = [];
-
-    //Create a loop o go through the density intervals and generate labels
-    for (var i = 0; i < grades.length; i++)
-    {
-      div.innerHTML +=
-        '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-        grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-    }
-    console.log('div' + div);
+  // Create a loop to go through the density intervals and generate labels
+  for (var i = 0; i < grades.length; i++) {
+    div.innerHTML +=
+      '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+      grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+  }
   return div;
 };
 
-
-// Create function to set color based on earthquake magnitudels
-
-function getColor(c)
-{
+// Create function to set color based on earthquake magnitude
+function getColor(c) {
   x = Math.ceil(c);
   switch (Math.ceil(x)) {
     case 1:
@@ -53,20 +45,20 @@ function getColor(c)
 
 // Create function to create circular features
 function createFeatures(earthquakeData) {
-  var earthquakes = L.geoJson(earthquakeData,{
+  var earthquakes = L.geoJson(earthquakeData, {
     pointToLayer: function (feature, latlng) {
       return L.circleMarker(latlng, {
-        radius: feature.properties.mag*3,
+        radius: feature.properties.mag * 3,
         fillColor: getColor(feature.properties.mag),
         color: "#000",
         weight: 1,
         opacity: 1,
-        fillOpacity: 0.8})
-        .bindPopup("<h3>" + "Location: " + feature.properties.place +
-          "</h3><hr><p>" + "Date/Time: " + new Date (feature.properties.time) + "<br>" + 
-          "Magnitude: " + feature.properties.mag + "</p>");
-  }
-});
+        fillOpacity: 0.8
+      }).bindPopup("<h3>Location: " + feature.properties.place +
+        "</h3><hr><p>Date/Time: " + new Date(feature.properties.time) + "<br>" +
+        "Magnitude: " + feature.properties.mag + "</p>");
+    }
+  });
 
   // Sending our earthquakes layer to the createMap function
   createMap(earthquakes);
